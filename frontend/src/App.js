69d2cf702ecb05c0432d422710fa05cf8c7daff3
smitgol <smitgol007@gmail.com>
2023-12-10 03:10:53 +0530

    huddle fixes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,21 @@ import {
 } from "react-router-dom";
 import routes from "./routes";
 import { Nav } from "./widgets/layout/nav";
+import { HuddleProvider, HuddleClient } from '@huddle01/react';
 
 const router12 = createBrowserRouter(routes);
+
+const huddleClient = new HuddleClient({
+  projectId: process.env.REACT_HUDDLE_PRIVATE_PROJECT_ID,
+});
 
 function App() {
   return (
     <>
-      <Nav/>
-      <RouterProvider router={router12} />
+      <HuddleProvider client={huddleClient}>
+        <Nav/>
+        <RouterProvider router={router12} />
+      </HuddleProvider>
     </>
   );
 }
