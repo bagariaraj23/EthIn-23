23f112e9dabdfdc2363374855ad5fba29d0f583c
smitgol <smitgol007@gmail.com>
2023-12-10 05:16:07 +0530

    added call api func

--- frontend/src/pages/Huddle.jsx
+++ frontend/src/pages/Huddle.jsx
@@ -113,7 +113,27 @@ const HuddleCom = () => {
         const newPatientArray = JSON.parse(patientTranscript).map(obj => ({ ...obj, ["User"]: "Patient" }));
         const mergedAndSortedArray = [...newTherapistArray, ...newPatientArray].sort((a, b) => a.startTime - b.startTime);
         console.log(mergedAndSortedArray);
-    }
+
+        const callDataToGPT = (hostTranscript, patientTranscript) => {
+            console.log(hostTranscript, patientTranscript);
+            const inputData = {
+                input: "Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%",
+                steps: [
+                    {
+                        skill: "summarize"
+                    }
+                ]
+              };
+          
+              fetch("https://api.oneai.com/api/v0/pipeline", {method:"POST", headers: {
+                "Content-Type": "application/json",
+                "api-key": "437f77b0-2e4b-4597-8e20-a87ffaf10762"
+              },body: JSON.stringify(inputData)})
+              .then(response => response.text())
+              .then(result => console.log(result))
+              .catch(error => console.log('error', error));
+            }
+        }
     const sendDataFunc = () => {
         SpeechRecognition.stopListening();
         if (peerIds.length > 0) {
